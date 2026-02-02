ae6490de0637bff40e143a688b78e5e8ac42e681
Trilleo <gongsanle@gmail.com>
2026-02-02 21:42:21 +0800

    Internal: Initial commit

---
+++ src/main/java/net/trilleo/rhythmcraft/RhythmCraft.java
@@ -0,0 +1,15 @@
+package net.trilleo.rhythmcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RhythmCraft implements ModInitializer {
+	public static final String MOD_ID = "rhythmcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/trilleo/rhythmcraft/RhythmCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.trilleo.rhythmcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RhythmCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
