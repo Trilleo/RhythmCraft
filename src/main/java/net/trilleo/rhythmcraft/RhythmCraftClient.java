b7be9bfa737295e0a61f90f0fa2467d7ef5265df
Trilleo <gongsanle@gmail.com>
2026-02-02 21:57:30 +0800

    Internal: Client mod setup

---
+++ src/main/java/net/trilleo/rhythmcraft/RhythmCraftClient.java
@@ -0,0 +1,10 @@
+package net.trilleo.rhythmcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RhythmCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
